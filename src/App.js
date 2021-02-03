ee33fd99f93fc173d9ba15c2f6e10b4fccad97c5
Edgar Arevalo <ea239720@gmail.com>
2021-02-03 16:45:59 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from './App.module.css';
+
+import Tareas from './Archivos/Tareas.json';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: []
+        }
+    }
+
+    date = (d) => {
+        var q = new Date(d);
+        return parseInt(
+            q.getFullYear().toString().concat(
+                '0'.concat(
+                    (q.getMonth() + 1).toString().concat(
+                        '0'.concat(
+                            q.getDate().toString()
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    componentDidMount() {
+        this.setState({
+            data: Tareas.tareas
+        });
+    }
+
+    render() {
+        var p = new Date();
+        var today = parseInt(
+                        p.getFullYear().toString().concat(
+                            '0'.concat(
+                                (p.getMonth() + 1).toString().concat(
+                                    '0'.concat(
+                                        p.getDate().toString()
+                                    )
+                                )
+                            )
+                        )
+                    );
+
+        return(
+            <div className = { styles.body }>
+                <table className = { styles.table }>
+                    <tr>
+                        <th>Fecha Peticion</th>
+                        <th>Fecha Entrega</th>
+                        <th>Materia</th>
+                        <th>Descripcion</th>
+                        <th>Plataforma</th>
+                    </tr>
+                    {
+                        this.state.data.sort((a, b) => (a.entrega > b.entrega) ? 1 : -1).map((tarea, id) => (
+                            (today - this.date(tarea.entrega) < -5) ? (
+                                <tr key = { id } className = { styles.green }>
+                                    <td>{ tarea.peticion }</td>
+                                    <td>{ tarea.entrega }</td>
+                                    <td>{ tarea.materia }</td>
+                                    <td>{ tarea.descripcion }</td>
+                                    <td><a href={ tarea.enlace }>{ tarea.plataforma }</a></td>
+                                </tr> ) :
+                                (today - this.date(tarea.entrega) < 0) ? (
+                                    <tr key = { id } className = { styles.yellow }>
+                                        <td>{ tarea.peticion }</td>
+                                        <td>{ tarea.entrega }</td>
+                                        <td>{ tarea.materia }</td>
+                                        <td>{ tarea.descripcion }</td>
+                                        <td><a href={ tarea.enlace }>{ tarea.plataforma }</a></td>
+                                    </tr> ) :
+                                    (today - this.date(tarea.entrega) === 0 ) ? (
+                                        <tr key = { id } className = { styles.red }>
+                                            <td>{ tarea.peticion }</td>
+                                            <td>{ tarea.entrega }</td>
+                                            <td>{ tarea.materia }</td>
+                                            <td>{ tarea.descripcion }</td>
+                                            <td><a href={ tarea.enlace }>{ tarea.plataforma }</a></td>
+                                        </tr> ) : (
+                                        <tr key = { id } className = { styles.lost }>
+                                            <td>{ tarea.peticion }</td>
+                                            <td>{ tarea.entrega }</td>
+                                            <td>{ tarea.materia }</td>
+                                            <td>{ tarea.descripcion }</td>
+                                            <td><a href={ tarea.enlace }>{ tarea.plataforma }</a></td>
+                                        </tr> )
+                        ))
+                    }
+                </table>
+            </div>
+        )
+    }
 }
-
-export default App;
